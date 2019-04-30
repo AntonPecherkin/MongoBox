321bd0595917fa0f0f60a80c399b5a052e106d22
Иван <if1242@yandex.ru>
2019-04-30 23:23:07 +0300

    nucypher

--- Web/mongo/app.py
+++ Web/mongo/app.py
@@ -6,10 +6,51 @@ import shutil
 import sys
 import json
 import struct
+import maya
+from twisted.logger import globalLogPublisher
+from nucypher.characters.lawful import Alice, Bob, Ursula
+from nucypher.data_sources import DataSource as Enrico
+from nucypher.network.middleware import RestMiddleware
+from nucypher.utilities.logging import simpleObserver
+from umbral.keys import UmbralPublicKey
 from time import sleep
 from flask import Flask
 from flask import request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
+
+# Execute the download script (download_finnegans_wake.sh) to retrieve the book
+BOOK_PATH = os.path.join('.', 'finnegans-wake.txt')
+
+# Twisted Logger
+globalLogPublisher.addObserver(simpleObserver)
+
+# Temporary file storage
+TEMP_FILES_DIR = "{}/examples-runtime-cruft".format(os.path.dirname(os.path.abspath(__file__)))
+TEMP_DEMO_DIR = "{}/finnegans-wake-demo".format(TEMP_FILES_DIR)
+TEMP_CERTIFICATE_DIR = "{}/certs".format(TEMP_DEMO_DIR)
+
+# Remove previous demo files and create new ones
+shutil.rmtree(TEMP_FILES_DIR, ignore_errors=True)
+os.mkdir(TEMP_FILES_DIR)
+os.mkdir(TEMP_DEMO_DIR)
+os.mkdir(TEMP_CERTIFICATE_DIR)
+
+#######################################
+# Finnegan's Wake on NuCypher Testnet #
+# (will fail with bad connection) #####
+#######################################
+
+TESTNET_LOAD_BALANCER = "eu-federated-balancer-40be4480ec380cd7.elb.eu-central-1.amazonaws.com"
+
+##############################################
+# Ursula, the Untrusted Re-Encryption Proxy  #
+##############################################
+ursula = Ursula.from_seed_and_stake_info(host=TESTNET_LOAD_BALANCER,
+                                         certificates_directory=TEMP_CERTIFICATE_DIR,
+                                         federated_only=True,
+                                         minimum_stake=0)
+
+
 
 UPLOAD_FOLDER = './static/uploads'
 app = Flask(__name__)
@@ -38,6 +79,36 @@ def sharedoc():
     doc = request.form['Doc']
     docname = request.form['Docname']
     owner = request.form['Owner']
+    policy_end_datetime = maya.now() + datetime.timedelta(days=5)
+    m, n = 2, 3  
+    label = b"secret/files/and/stuff"
+    ######################################
+    # Alice, the Authority of the Policy # 
+    ######################################
+    ALICE = Alice(network_middleware=RestMiddleware(),
+                  known_nodes=[ursula],
+                  learn_on_same_thread=True,
+                  federated_only=True,
+                  known_certificates_dir=TEMP_CERTIFICATE_DIR)      
+    BOB = Bob(known_nodes=[ursula],
+              network_middleware=RestMiddleware(),
+              federated_only=True,
+              start_learning_now=True,
+              learn_on_same_thread=True,
+              known_certificates_dir=TEMP_CERTIFICATE_DIR)
+
+    ALICE.start_learning_loop(now=True)
+    policy = ALICE.grant(BOB,
+                         label,
+                         m=m, n=n,
+                         expiration=policy_end_datetime)
+
+    # Alice puts her public key somewhere for Bob to find later...
+    alices_pubkey_bytes_saved_for_posterity = bytes(ALICE.stamp)
+    # ...and then disappears from the internet.    
+    del ALICE
+     
+    BOB.join_policy(label, alices_pubkey_bytes_saved_for_posterity)
     return render_template('main.html', share=share, upload=upload, time=time, user=user, login=login, doc=doc, docname=docname, owner=owner)
 
 @app.route("/upload", methods=['POST'])
